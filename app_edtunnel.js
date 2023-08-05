8e4592a1caae9a4e63c98e9cb80944f59bebf13d
zzmszz <141062092+zzmszz@users.noreply.github.com>
2023-08-05 16:58:23 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'f36d9993-b605-494d-a19a-7cd8c21ff388';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
